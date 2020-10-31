17019a07b23b4d06c5592ecc161e784af2a5cc37
Crytec3 <68275525+Crytec3@users.noreply.github.com>
2020-10-31 10:51:23 +0100

    next commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 
 
 public class Main {
-    Scanner sc = new Scanner(System.in);
-    Random rand = new Random();
+    static Scanner sc = new Scanner(System.in);
+    static Random rand = new Random();
 
-    int water = 400;
-    int milk = 540;
-    int beans = 120;
-    int cups = 9;
-    int money = 550;
-    boolean exit = false;
+    static int water = 400;
+    static int milk = 540;
+    static int beans = 120;
+    static int cups = 9;
+    static int money = 550;
+    static boolean exit = false;
 
-    public void stats() {
+    public static void stats() {
         System.out.println("The coffee machine has:");
         System.out.println(water + " of water");
         System.out.println(milk + " of milk");
@@ -24,10 +24,11 @@ public class Main {
         System.out.println("$" + money + " of money");
     }
 
-    public void artwork() {
-
+    public static void artworkCoffee() {
         int num = rand.nextInt(4);
+
         System.out.println("I have enough resources, making you a coffee!\n");
+
         switch (num) {
             case 0 -> {
                 System.out.println("                        (");
@@ -101,10 +102,38 @@ public class Main {
                 System.out.println("    '-------'\n");
             }
         }
-        System.out.println("Here`s your Coffee!\n");
+        System.out.println("Here's your Coffee!\n");
     }
 
-    public void changeStats (int currentWater, int currentMilk, int currentBeans, int currentMoney){
+    public static void artworkTea() {
+        int num = rand.nextInt(2);
+
+        System.out.println("I have enough resources, making you a Tea!\n");
+
+        switch (num){
+            case 0 -> {
+                System.out.println("      )");
+                System.out.println("     (");
+                System.out.println("      )");
+                System.out.println("  _.-~(~-.");
+                System.out.println(" (@\\`---'/.  ");
+                System.out.println("('  `._.'  `)");
+                System.out.println(" `-..___..-'");
+            }
+            case 1 -> {
+                System.out.println("            )");
+                System.out.println("           (");
+                System.out.println("            )");
+                System.out.println(".-.,--^--.  _");
+                System.out.println("\\\\| `---' |//");
+                System.out.println(" \\|        /");
+                System.out.println("_\\_______/_");
+            }
+        }
+        System.out.println("Here's your Tea!");
+    }
+
+    public static void changeStats(int currentWater, int currentMilk, int currentBeans, int currentMoney){
         water -= currentWater;
         milk -= currentMilk;
         beans -= currentBeans;
@@ -112,7 +141,7 @@ public class Main {
         cups -= 1;
     }
 
-    public void fill(){
+    public static void fill(){
         System.out.println("Write how many ml of water do you want to add:");
         water += sc.nextInt();
         System.out.println("Write how many ml of milk do you want to add:");
@@ -123,7 +152,7 @@ public class Main {
         cups += sc.nextInt();
     }
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Hello I'm Carl your virtual Coffee Machine.\n");
@@ -134,37 +163,45 @@ public class Main {
             if (!action.equals("exit")) {
                 switch (action) {
                     case "buy" -> {
-                        System.out.println("What do you want? We got espresso (1), latte (2), cappuccino (3) or you can go back to main menu (back):");
+                        System.out.println("Do you want a Tea (1) or a Coffee (2):");
                         String select1 = sc.next();
-                        switch (select1) {
-                            case "1", "espresso" -> {
-                                if (water >= 250 && beans >= 16 && cups >= 1) {
-                                    changeStats(250, 0, 16, 4);
-                                    artwork(); }
-                                else if (water < 250) System.out.println("Sorry, not enough water!");
-                                else if (beans < 16) System.out.println("Sorry, not enough beans!");
-                                else System.out.println("Sorry, not enough cups!");
+                        if (select1.equals("1") || select1.equals("Tea") || select1.equals("tea")) {
+
+                        }
+                        else {
+                            System.out.println("What do you want? We got espresso (1), latte (2), cappuccino (3) or you can go back to main menu (back):");
+                            String select2 = sc.next();
+                            switch (select2) {
+                                case "1", "espresso" -> {
+                                    if (water >= 250 && beans >= 16 && cups >= 1) {
+                                        changeStats(250, 0, 16, 4);
+                                        artworkCoffee();
+                                    } else if (water < 250) System.out.println("Sorry, not enough water!");
+                                    else if (beans < 16) System.out.println("Sorry, not enough beans!");
+                                    else System.out.println("Sorry, not enough cups!");
+                                }
+                                case "2", "latte" -> {
+                                    if (water >= 350 && milk >= 75 && beans >= 20 && cups >= 1) {
+                                        changeStats(350, 75, 20, 7);
+                                        artworkCoffee();
+                                    } else if (water < 350) System.out.println("Sorry, not enough water!");
+                                    else if (milk < 75) System.out.println("Sorry not enough milk!");
+                                    else if (beans < 20) System.out.println("Sorry, not enough beans!");
+                                    else System.out.println("Sorry, not enough cups!");
+                                }
+                                case "3", "cappuccino" -> {
+                                    if (water >= 200 && milk >= 100 && beans >= 12 && cups >= 1) {
+                                        changeStats(200, 100, 12, 6);
+                                        artworkCoffee();
+                                    } else if (water < 200) System.out.println("Sorry, not enough water!");
+                                    else if (milk < 100) System.out.println("Sorry not enough milk!");
+                                    else if (beans < 12) System.out.println("Sorry, not enough beans!");
+                                    else System.out.println("Sorry, not enough cups!");
+                                }
+                                case "back" -> {
+                                }
+                                default -> System.out.println("Invalid input. Try again.");
                             }
-                            case "2", "latte" -> {
-                                if (water >= 350 && milk >= 75 && beans >= 20 && cups >= 1) {
-                                    changeStats(350,75,20,7);
-                                    artwork(); }
-                                else if (water < 350) System.out.println("Sorry, not enough water!");
-                                else if (milk < 75) System.out.println("Sorry not enough milk!");
-                                else if (beans < 20) System.out.println("Sorry, not enough beans!");
-                                else System.out.println("Sorry, not enough cups!");
-                            }
-                            case "3", "cappuccino" -> {
-                                if (water >= 200 && milk >= 100 && beans >= 12 && cups >= 1) {
-                                    changeStats(200,100,12,6);
-                                    artwork(); }
-                                else if (water < 200) System.out.println("Sorry, not enough water!");
-                                else if (milk < 100) System.out.println("Sorry not enough milk!");
-                                else if (beans < 12) System.out.println("Sorry, not enough beans!");
-                                else System.out.println("Sorry, not enough cups!");
-                            }
-                            case "back" -> {}
-                            default -> System.out.println("Invalid input. Try again.");
                         }
                     }
                     case "fill" -> fill();
